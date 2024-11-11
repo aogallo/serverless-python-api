b7bc011284ded14dd376417145bb9aa5b2ce42ad
Allan Gallo <allan.gallo.guerra@gmail.com>
2024-11-11 14:28:55 -0600

    add tracer logger and api gateway http resolver

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
 import json
-from aws_lambda_powertools.event_handler import APIGatewayRestResolver
+from aws_lambda_powertools import Logger, Tracer
+from aws_lambda_powertools.event_handler import APIGatewayHttpResolver
+from aws_lambda_powertools.utilities.typing import LambdaContext
+from aws_lambda_powertools.logging import correlation_paths
 
-app = APIGatewayRestResolver()
+logger = Logger()
+tracer = Tracer()
+app = APIGatewayHttpResolver()
+
+
+@app.get("/")
+@tracer.capture_method
+def ping():
+    return {"message": "pong"}
+
 
 @app.get("/hello/<name>")
 def hello_name(name):
-    return { "statusCode": 200, "body": json.dumps({"message": f"hello {name}"})}
+    return {"statusCode": 200, "body": {"message": f"hello {name}"}}
+
 
 @app.get("/hello")
 def hello():
@@ -13,10 +26,12 @@ def hello():
         "message": "Go Serverless v4.0! Your function executed successfully!",
     }
 
-    response = {"statusCode": 200, "body": json.dumps(body)}
+    response = {"statusCode": 200, "body": body}
 
     return response
 
 
-def lambda_handler(event, context):
-    return app.resolve(event,context)
+@logger.inject_lambda_context(correlation_id_path=correlation_paths.API_GATEWAY_HTTP)
+@tracer.capture_lambda_handler
+def lambda_handler(event: dict, context: LambdaContext) -> dict:
+    return app.resolve(event, context)
