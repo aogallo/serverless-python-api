6ab889e988e350a0cbbe96443f8d52ff2ceeeb91
Allan Gallo <allan.gallo.guerra@gmail.com>
2024-11-11 15:10:23 -0600

    adding metrics

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-import json
-from aws_lambda_powertools import Logger, Tracer
+from aws_lambda_powertools import Logger, Tracer, Metrics
 from aws_lambda_powertools.event_handler import APIGatewayHttpResolver
+from aws_lambda_powertools.metrics import MetricUnit
 from aws_lambda_powertools.utilities.typing import LambdaContext
 from aws_lambda_powertools.logging import correlation_paths
 
 logger = Logger()
 tracer = Tracer()
 app = APIGatewayHttpResolver()
+cold_start = True
+metrics = Metrics(namespace="MyApi", service="APP")
 
 
 @app.get("/")
@@ -17,6 +19,7 @@ def ping():
 
 @app.get("/hello/<name>")
 def hello_name(name):
+    metrics.add_metric(name="SuccessfullGreetings", unit=MetricUnit.Count, value=1)
     return {"statusCode": 200, "body": {"message": f"hello {name}"}}
 
 
@@ -26,6 +29,7 @@ def hello():
         "message": "Go Serverless v4.0! Your function executed successfully!",
     }
 
+    metrics.add_metric(name="SuccessfullGreetings", unit=MetricUnit.Count, value=1)
     response = {"statusCode": 200, "body": body}
 
     return response
@@ -33,5 +37,6 @@ def hello():
 
 @logger.inject_lambda_context(correlation_id_path=correlation_paths.API_GATEWAY_HTTP)
 @tracer.capture_lambda_handler
+@metrics.log_metrics(capture_cold_start_metric=True)
 def lambda_handler(event: dict, context: LambdaContext) -> dict:
     return app.resolve(event, context)
